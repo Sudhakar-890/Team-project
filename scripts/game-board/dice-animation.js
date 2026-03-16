1b4b3be2d905342325516ae6d5124ae3b5d01b1f
sudhakar-890 <sudhakarsuresh4321@gmail.com>
2026-03-16 23:00:28 +0530

    sudhakar2 branch added. new js files

--- scripts/game-board/dice-animation.js
+++ scripts/game-board/dice-animation.js
@@ -1,104 +1,75 @@
-import {calculateMoves} from './game.js';
+import { calculateMoves } from "./game.js";
 
+let currentPlayer = 0;
+let lastDice = 0;
 
-// change the dice as per player wise
-let diceIndex = 0;
-let playersDice = document.querySelectorAll('.player .diceSlot');
+const diceSlots = document.querySelectorAll(".diceSlot");
 
-let diceHTML =
- `
-    <div class="diceContainer">
-        <div class="dice">
-            <div class="face1">
-                <img src="./assets/dice/one.png">
-            </div>
-            <div class="face2">
-                <img src="./assets/dice/two.png">
-            </div>
-            <div class="face3">
-                <img src="./assets/dice/three.png">
-            </div>
-            <div class="face4">
-                <img src="./assets/dice/four.png">
-            </div>
-            <div class="face5">
-                <img src="./assets/dice/five.png">
-            </div>
-            <div class="face6">
-                <img src="./assets/dice/six.png">
-            </div>
-        </div>
-    </div>
+const diceHTML = `
+<div class="diceContainer">
+ <div class="dice">
+  <div class="face1"><img src="./assets/dice/one.png"></div>
+  <div class="face2"><img src="./assets/dice/two.png"></div>
+  <div class="face3"><img src="./assets/dice/three.png"></div>
+  <div class="face4"><img src="./assets/dice/four.png"></div>
+  <div class="face5"><img src="./assets/dice/five.png"></div>
+  <div class="face6"><img src="./assets/dice/six.png"></div>
+ </div>
+</div>
+`;
 
-    `;
+renderDice();
 
-displayDice(diceIndex);
+export function nextTurn() {
 
-function displayDice(value) {
- if (value >= 4) {
-  diceIndex = 0;
- }
- playersDice[diceIndex].insertAdjacentHTML('afterbegin', diceHTML);
- addDiceEvent();
+    if (lastDice !== 6) {
+        currentPlayer++;
+        if (currentPlayer > 3) currentPlayer = 0;
+    }
+
+    renderDice();
+
 }
 
-// dice roll
-let currentX = 0;
-let currentY = 0;
-function addDiceEvent() {
- const dice = document.querySelector(".dice");
- 
- dice.addEventListener('click', rollDice);
- 
- function rollDice() {
-   //const random = Math.floor(Math.random() * 6 + 1);
-  const random = 6;
-     console.log(random, 'dice number');
-  let x = 0;
-  let y = 0;
-  
-  switch (random) {
-   case 1:
-    break;
-    
-   case 2:
-    y = -90;
-    break;
-    
-   case 3:
-    y = 90;
-    break;
-    
-   case 4:
-    x = 180;
-    break;
-    
-   case 5:
-    x = -90;
-    break;
-    
-   case 6:
-    x = 90;
-    break;
-  }
-  
-  currentX += x + 720;
-  currentY += y + 720;
-  
-  dice.style.transform = `rotateX(${currentX}deg) rotateY(${currentY}deg)`;
-  
-  setTimeout(() => {
-   if (random != 6){
-    currentX = 0
-    currentY = 0;
-    playersDice[diceIndex].innerHTML = '';
-    diceIndex += 1;
-    
-    displayDice(diceIndex);
-   }
+function renderDice() {
 
-    //move coins function
-   calculateMoves(diceIndex,random);
-  }, 1700)
- }
+    diceSlots.forEach(s => s.innerHTML = "");
+
+    diceSlots[currentPlayer].insertAdjacentHTML("afterbegin", diceHTML);
+
+    const dice = diceSlots[currentPlayer].querySelector(".dice");
+
+    dice.addEventListener("click", rollDice);
+
+}
+
+function rollDice() {
+
+    const dice = diceSlots[currentPlayer].querySelector(".dice");
+
+    const random = Math.floor(Math.random() * 6) + 1;
+
+    lastDice = random;
+
+    let x = 0, y = 0;
+
+    switch (random) {
+        case 2: y = -90; break;
+        case 3: y = 90; break;
+        case 4: x = 180; break;
+        case 5: x = -90; break;
+        case 6: x = 90; break;
+    }
+
+    dice.style.transform = `rotateX(${x}deg) rotateY(${y}deg)`;
+    dice.style.animation = "extraSpin 1s ease";
+
+    setTimeout(() => {
+
+        dice.style.animation = "";
+
+        calculateMoves(currentPlayer, random);
+
+    }, 1200);
+
 }
--- scripts/game-board/game.js
+++ scripts/game-board/game.js
@@ -1,125 +1,127 @@
-let player1 = {
-    coin1: 0,
-    coin2: 0,
-    coin3: 0,
-    coin4: 0, reachedOrigin: false };
+import { nextTurn } from "./dice-animation.js";
 
-let player2 = {
-    coin1: 0,
-    coin2: 0,
-    coin3: 0,
-    coin4: 0, reachedOrigin: false };
+const players = [
+    { coin1: 0, coin2: 0, coin3: 0, coin4: 0 },
+    { coin1: 0, coin2: 0, coin3: 0, coin4: 0 },
+    { coin1: 0, coin2: 0, coin3: 0, coin4: 0 },
+    { coin1: 0, coin2: 0, coin3: 0, coin4: 0 }
+];
 
-let player3 = {
-    coin1: 0,
-    coin2: 0,
-    coin3: 0,
-    coin4: 0, reachedOrigin: false };
+const coinNames = ["coin1", "coin2", "coin3", "coin4"];
 
-let player4 = { 
-    coin1: 0,
-    coin2: 0, 
-    coin3: 0, 
-    coin4: 0, reachedOrigin:false };
+const origin = [1, 14, 27, 40];
 
-let coinDetails = {
-    player1 : {origin:1,home:51},
-    player2 : {origin:14,home:12},
-    player3 : {origin:27,home:25},
-    player4 : {origin:40,home:38}
-}
+export function calculateMoves(playerIndex, diceValue) {
+    const coins = document.querySelectorAll(`.coin${playerIndex + 1}Img`);
 
-const GamePlayers = [player1,player2,player3,player4];
-let preventRunAgain = false;
+    let movableCoins = [];
 
-export function calculateMoves(diceIndex,random){
-    let handlers = [];
-    let coinsImg = document.querySelectorAll('.coin'+(diceIndex+1)+'Img');
-    const findCoin = ['coin1', 'coin2', 'coin3', 'coin4'];
+    // STEP 1: Determine movable coins
+    console.log(coins)
+    coins.forEach((coin, i) => {
 
-    GamePlayers.forEach((player,index1)=>{
-        let targetCoins = Object.entries(player);
-        if(index1===diceIndex){
-            if(random===6 && !player.reachedOrigin){
-             const controller = new AbortController();
-                targetCoins.forEach((coin,index2)=>{
-                 console.log(targetCoins[4]);
-                    if(index2<4 && coin[1]==0){
-                        coinsImg[index2].classList.add('selectCoin');
-                       /* const handler = () => { enterOrigin(index1, index2); }
-                        handlers.push(handler);*/
-                        console.log('first if', index2);
-                        coinsImg[index2].addEventListener('click',()=>{enterOrigin(index1,index2);controller.abort();},{signal:controller.signal});
-                    }
-                    else if(index2<4){
-                        coinsImg[index2].classList.add('selectCoin');
-                       /* const handler = () => moveCoin(index1, index2);
-                        handlers.push(handler);*/
-                        console.log('second if',index2);
-                        coinsImg[index2].addEventListener('click',()=>{moveCoin(index1,index2);controller.abort();},{signal:controller.signal});
-                    }
-                });
+        const coinName = coinNames[i];
+        const position = players[playerIndex][coinName];
+        
+        // coin in base
+        if (position === 0) {
+
+            if (diceValue === 6) {
+                movableCoins.push(i);
             }
 
-            else{
-                targetCoins.forEach((coin,index2) => {
-                    coinsImg[index2].classList.add('selectCoin');
-                    /*let handler = () => moveCoin(index1, index2);
-                    handlers[index2] = handler;*/
-                    coinsImg[index2].addEventListener('click',moveCoin,{signal:controller.signal});
-                });
-            }
         }
+
+        // coin already on board
+        else {
+
+            movableCoins.push(i);
+
+        }
+
     });
 
-    // enter coin into origin
-    function enterOrigin(playerIndex,coinIndex){
-    
-        GamePlayers[playerIndex][findCoin[coinIndex]] = 1;
-        let reachedOriginCount = 0;
-       /* handlers.forEach((handler,index)=>{
-            coinsImg[index].removeEventListener('click', handler);
-        })*/
-        coinsImg.forEach((coinImg,index)=>{
-            if(index===coinIndex){
-                console.log(coinIndex)
-                coinImg.classList.add('movingCoin');
-                document.querySelector('.index-1').appendChild(coinImg);
-            }
-            if (GamePlayers[playerIndex][findCoin[index]]!=0){
-                reachedOriginCount ++;
-                console.log(reachedOriginCount,'reachedOriginCount');
-            }
-            coinImg.classList.remove('selectCoin');
-        });
-
-        if(reachedOriginCount===4){
-            GamePlayers[playerIndex]['reachedOrigin'] = true;
-            console.log('reachedOrigin all');
-        }
-        console.log(GamePlayers);
+    // STEP 2: No possible move
+    if (movableCoins.length === 0) {
+        setTimeout(nextTurn, 800);
+        return;
     }
 
-    // move coins by rand number
-    function moveCoin(playerIndex,coinIndex){
-        console.log(coinIndex)
-        GamePlayers[playerIndex][findCoin[coinIndex]] += random;
-        const coinStep = GamePlayers[playerIndex][findCoin[coinIndex]];
+    // STEP 3: Highlight movable coins
+    movableCoins.forEach((i,movableCoin) => {
+        const coin = movableCoin;
+        console.log(i,)
+        coin.classList.add("selectCoin");
 
-       /* handlers.forEach((handler,index)=>{
-            coinsImg[index].removeEventListener('click', handler);
-        });*/
+        coin.onclick = () => {
 
-        console.log('move coin function');
-        coinsImg.forEach((coinImg,index) => {
-            if(index===coinIndex){
-                console.log(document.querySelector(`.index-${coinStep}`));
+            const coinName = coinNames[i];
+            const current = players[playerIndex][coinName];
 
-                document.querySelector(`.index-${coinStep}`).appendChild(coinImg);
+            if (current === 0) {
+
+                enterBoard(playerIndex, i);
+
             }
-            coinsImg[index].classList.remove('selectCoin');
-        });
+            else {
 
-        console.log(GamePlayers)
-    }
+                moveCoin(playerIndex, i, diceValue);
+
+            }
+
+            clearSelection(playerIndex);
+
+            nextTurn();
+
+        };
+
+    });
+
+}
+
+function enterBoard(playerIndex, coinIndex) {
+
+    const coins = document.querySelectorAll(`.coin${playerIndex + 1}Img`);
+
+    const coin = coins[coinIndex];
+
+    const coinName = coinNames[coinIndex];
+
+    const start = origin[playerIndex];
+
+    players[playerIndex][coinName] = start;
+
+    document.querySelector(`.index-${start}`).appendChild(coin);
+
+}
+
+function moveCoin(playerIndex, coinIndex, diceValue) {
+
+    const coins = document.querySelectorAll(`.coin${playerIndex + 1}Img`);
+
+    const coin = coins[coinIndex];
+
+    const coinName = coinNames[coinIndex];
+
+    let current = players[playerIndex][coinName];
+
+    let next = current + diceValue;
+
+    if (next > 52) next = next - 52;
+
+    players[playerIndex][coinName] = next;
+
+    document.querySelector(`.index-${next}`).appendChild(coin);
+
+}
+
+function clearSelection(playerIndex) {
+
+    const coins = document.querySelectorAll(`.coin${playerIndex + 1}Img`);
+
+    coins.forEach(c => {
+        c.classList.remove("selectCoin");
+        c.onclick = null;
+    });
+
 }
