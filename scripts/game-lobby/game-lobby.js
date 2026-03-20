97f51b55e3791d7c3a944f5af3ded7835cf4f1cb
Sudhakar-890 <sudhakarsuresh4321@gmail.com>
2026-03-20 23:24:46 +0530

    updated the js window location path

--- scripts/game-lobby/game-lobby.js
+++ scripts/game-lobby/game-lobby.js
@@ -1,23 +1,23 @@
 
 document.addEventListener('click',(e)=>{
     if(e.target.closest('.gameHistory')){
-        window.location.href = '/history.html';
+        window.location.href = 'Team-project/history.html';
     }
 
     if(e.target.closest('.gameLobby')){
-        window.location.href = '/game-lobby.html';
+        window.location.href = 'Team-project/game-lobby.html';
     }
 
     if (e.target.closest('.themeBoard')) {
-        window.location.href = '/board-theme.html';
+        window.location.href = 'Team-project/board-theme.html';
     }
     
     if(e.target.closest('.computerMatch')){
-        window.location.href = '/computer.html';
+        window.location.href = 'Team-project/computer.html';
     }
     
     if(e.target.closest('.localMatch')){
-        window.location.href = '/game-board.html';
+        window.location.href = 'Team-project/game-board.html';
     }
 })
 
--- scripts/sign-up/sign-up.js
+++ scripts/sign-up/sign-up.js
@@ -1,7 +1,7 @@
 document.querySelector('.button').onclick = () =>{
-    window.location.href = '/game-lobby.html';
+    window.location.href = 'Team-project/game-lobby.html';
 }
 
 document.querySelector('.text2').onclick = () =>{
-    window.location.href = '/Index.html';
+    window.location.href = 'Team-project/Index.html';
 }
