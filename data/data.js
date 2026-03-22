6def0cbec5322376eee1ab82b617240edc9721d2
sudhakar-890 <sudhakarsuresh4321@gmail.com>
2026-03-22 10:24:32 +0530

    sign in page verify done and data.js started to write

--- data/data.js
+++ data/data.js
@@ -1,5 +1,20 @@
-let history = JSON.parse(localStorage.getItem('game-history'));
+export let userData = JSON.parse(localStorage.getItem('userData'))
+ ||
+[{
+    userEmail : 'hello',
+    userPassword : '123'
+}];
 
-saveToStorage(){
- 
+export function saveCurrentUser(verify){
+    localStorage.setItem('currentUser', JSON.stringify(verify));
+    getCurrentUser();
+}
+
+
+// get the current user from localStorage
+let currentUser = [];
+
+export function getCurrentUser(){
+    currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    console.log(currentUser);
 }
--- scripts/sign-in/sign-in.js
+++ scripts/sign-in/sign-in.js
@@ -1,2 +1,41 @@
+import { userData,saveCurrentUser } from "../../data/data.js";
 
+document.addEventListener('DOMContentLoaded',()=>{
+    console.log('loaded')
+    const email = document.querySelector('#emailBar input');
+    const password = document.querySelector('#passwordBar input');
+
+    const signinBtn = document.querySelector('#signinBar button');
+
+    signinBtn.addEventListener('click', () => {
+        console.log('sign in clicked')
+        let emailText = email.value;
+        let passwordText = password.value;
+        console.log(emailText,passwordText)
+
+        let verify = userData.filter((user) => {
+            console.log(user.userEmail === emailText, user.userPassword === passwordText)
+            if (user.userEmail === emailText && user.userPassword === passwordText) {
+                return [emailText,passwordText];
+            }
+        })
+
+        saveCurrentUser(verify);
+
+        setTimeout(()=>{
+            if (verify.length ==1) {
+                email.value = '';
+                password.value = '';
+                window.location.href = '/game-lobby.html';
+            }
+
+            else {
+                window.alert('Invalid userID , Password !')
+                email.value = '';
+                password.value ='';
+            }
+        },500)
+
+    })
+})
 
